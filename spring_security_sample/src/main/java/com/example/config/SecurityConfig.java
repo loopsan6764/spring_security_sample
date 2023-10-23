1355353abefdb5c840ffb397dbd60ddc76500115
Kohki Homma <loop.skydriver@hotmail.co.jp>
2023-10-23 15:00:15 +0900

    SpringBootアプリケーションの実行ができない。

--- spring_security_sample/src/main/java/com/example/config/SecurityConfig.java
+++ spring_security_sample/src/main/java/com/example/config/SecurityConfig.java
@@ -16,39 +16,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // 認証・認可に関する設定を追加します
-    	// 認可の設定
-        http.authorizeRequests()
-            .antMatchers("/loginForm").permitAll() // /loginFormは、全ユーザからのアクセスを許可
-            .anyRequest().authenticated(); // 許可した項目以外は、認証を求める
-    }
-    
-    @Override
-    // WebSecurity型の引数を持ったconfigure()を追加します
-    public void configure(WebSecurity web) throws Exception {
         /** 以下のファイルパス配下のディレクトリ、ファイルすべてを認証・認可の対象から除外します
-            src/main/resources/static/css/
-            src/main/resources/static/js/
-            src/main/resources/static/images/
-        */
-        web.ignoring().antMatchers("/css/**", "/js/**", "/images/**");
+        src/main/resources/static/css/
+        src/main/resources/static/js/
+        src/main/resources/static/images/
+    */
+    web.ignoring().antMatchers("/css/**", "/js/**", "/images/**");
     }
     
+    //ログイン処理
     http.formLogin()
-    	.loginProcessingUrl("/login") // ログイン処理のパス
-    	.loginPage("/loginForm") // ログインページの指定
-    	.usernameParameter("email") // ログインページのメールアドレス
-    	.passwordParameter("password") // ログインページのパスワード
-    	.defaultSuccessUrl("/home", true) // ログイン成功後のパス
-    	.failureUrl("/loginForm?error"); // ログイン失敗時のパス
+        .loginProcessingUrl("/login") // ログイン処理のパス
+        .loginPage("/loginForm") // ログインページの指定
+        .usernameParameter("email") // ログインページのメールアドレス
+        .passwordParameter("password") // ログインページのパスワード
+        .defaultSuccessUrl("/home", true) // ログイン成功後のパス
+        .failureUrl("/loginForm?error"); // ログイン失敗時のパス
 }
 
-//ログアウト処理
-http.logout()
-    .logoutUrl("/logout") //ログアウト処理のパス
-    .logoutSuccessUrl("/loginForm"); //ログアウト成功後のパス
-}
-@Bean
-public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-}
+	//ログアウト処理
+	http.logout()
+	    .logoutUrl("/logout") //ログアウト処理のパス
+	    .logoutSuccessUrl("/loginForm"); //ログアウト成功後のパス
+	}
+	
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
